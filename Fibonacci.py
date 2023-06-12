cc4aa14325ea8fdfdd78ffee6dfafc8ea3c293d1
Harshit Mittal <68640823+Iamtheonlyking1@users.noreply.github.com>
2023-06-13 04:18:53 +0530

    Update Fibonacci.py
    the complete code

--- Fibonacci.py
+++ Fibonacci.py
@@ -1,1 +1,18 @@
-print("Hello")
+num = int(input("How many numbers: "))
+
+n1 =  0
+n2 = 1
+count = 0
+
+if num <= 0:
+    print("ERROR")
+elif num == 1:
+    print(n1)
+else:
+    while count < num:
+        print(n1)
+        x = n1 + n2
+        n1 = n2
+        n2 = x
+        count += 1
+        
